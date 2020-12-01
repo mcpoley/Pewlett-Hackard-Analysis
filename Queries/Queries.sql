b1748766d5170700706cf82936cc5667962ea378
Mark <Mark@MacBook-Pro-2.attlocal.net>
2020-11-30 22:14:35 -0500

    queries so far

---
+++ Queries/Queries.sql
@@ -0,0 +1,7 @@
+--Retirement eligibility
+SELECT COUNT (first_name)
+FROM employee
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+SELECT * FROM retirement_info
